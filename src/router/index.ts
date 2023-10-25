b826492b60a26ba4bc4ffa417764cbb1d12e7db5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-25 15:31:50 +0800

    feat: add route views

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,21 @@ const router = createRouter({
             name: 'home',
             component: () => import('../views/HomeView.vue'),
         },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/AboutView.vue'),
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: () => import('../views/ProjectsView.vue'),
+        },
+        {
+            path: '/blog',
+            name: 'blog',
+            component: () => import('../views/BlogView.vue'),
+        },
     ],
 })
 
