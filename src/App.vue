e4e9606965231fcc79e02795a4f35ac535a2ff99
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-25 15:31:50 +0800

    feat: add route views

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { Shell } from 'lucide-vue-next'
 </script>
 
 <template>
     <header>
+        <nav>
+            <RouterLink to="/">
+                <Shell class="logo" :size="60" />
+            </RouterLink>
+            <div class="links">
                 <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/projects">Projects</RouterLink>
+                <RouterLink to="/blog">Blog</RouterLink>
+            </div>
+        </nav>
     </header>
 
+    <main>
+        <RouterView />
+    </main>
 </template>
 
 nav {
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-    <h1>Hello World</h1>
+    <h1>Under construction</h1>
 </template>
