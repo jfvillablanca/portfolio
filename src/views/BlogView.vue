4500f4a19b0c37b44902d6a06008069ac67f4e27
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-26 12:41:16 +0800

    feat: add section-headers to about, blog, projects

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
+    <h1 class="section-header">Blog</h1>
     <h1>Under construction</h1>
 </template>
+
+<style scoped lang="scss">
+.section-header {
+    margin-block: 2rem;
+
+    font-size: 4rem;
+}
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-    <h1>Under construction</h1>
+    <h1 class="section-header">Recent Works</h1>
 </template>
+
+<style scoped lang="scss">
+.section-header {
+    margin-block: 2rem;
+
+    font-size: 4rem;
+}
+</style>
