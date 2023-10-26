699e0915df3e9fb32253475b74dc83f1f4e46711
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-26 13:27:54 +0800

    feat: populate HomeView with details

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,38 @@
 <script setup lang="ts"></script>
 
 <template>
-    <h1>Hello World</h1>
+    <div class="container">
+        <h3>Hello, I am</h3>
+        <h1>Jann Marc</h1>
+        <div class="statement">
+            <p class="mr-4">I turn ideas into lines of code one</p>
+            <span class="font-mono px-4 py-2 rounded-2xl bg-[#eff1f3] dark:bg-[#343941]"
+                >git commit</span
+            >
+            <p class="ml-4">at a time</p>
+        </div>
+    </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+    margin-top: 20%;
+    display: grid;
+    place-items: center;
+
+    h1 {
+        font-size: 10rem;
+    }
+
+    .statement {
+        font-size: 3rem;
+
+        display: flex;
+        align-items: center;
+    }
+
+    h3 {
+        font-size: 4rem;
+    }
+}
+</style>
