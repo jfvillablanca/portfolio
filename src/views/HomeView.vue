921c62d98997b5ce291db7ac0fd05e8bf858d38f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-30 12:18:12 +0800

    remove statement, replace with standard title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,15 @@
         <h3>Hello, I am</h3>
         <h1>Jann Marc</h1>
         <div class="statement">
-            <p class="mr-4">I turn ideas into lines of code one</p>
-            <span class="font-mono px-4 py-2 rounded-2xl bg-[#eff1f3] dark:bg-[#343941]"
-                >git commit</span
-            >
-            <p class="ml-4">at a time</p>
+            <!-- <p class="mr-4">I turn ideas into lines of code one</p> -->
+            <!-- <span class="font-mono px-4 py-2 rounded-2xl bg-[#eff1f3] dark:bg-[#343941]"> -->
+            <!--     git commit -->
+            <!-- </span> -->
+            <!-- <p class="ml-4">at a time</p> -->
+
+            <p class="font-mono px-4 py-2 rounded-2xl bg-[#eff1f3] dark:bg-[#343941]">
+                Software Developer
+            </p>
         </div>
     </div>
 </template>
